e1633a05a4491b3f6a6f1acfec1ddccc35fcb10b
Rivan Chandra <rivan@genesisdigital.asia>
2023-06-15 23:10:44 +0700

    add loading in search
    Update: add loading animation during fetch API

--- src/components/SearchTextField.tsx
+++ src/components/SearchTextField.tsx
@@ -3,6 +3,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import IconButton from '@mui/material/IconButton';
 
@@ -10,6 +11,7 @@ interface SearchTextFieldProps {
   searchText: string;
   handleChangeSearch: (event: ChangeEvent<HTMLInputElement>) => void;
   searchFunction: () => void;
+  searchLoading: boolean;
 }
 
 export const SearchTextField: React.FC<SearchTextFieldProps> = (props) => {
@@ -23,9 +25,13 @@ export const SearchTextField: React.FC<SearchTextFieldProps> = (props) => {
         InputProps={{
           endAdornment: (
             <InputAdornment position="end">
-              <IconButton color="primary" aria-label="upload picture" component="label" onClick={props.searchFunction}>
-                <SearchIcon />
-              </IconButton>
+              {props.searchLoading?
+                <CircularProgress size={18}/>
+              :
+                <IconButton color="primary" aria-label="upload picture" component="label" onClick={props.searchFunction}>
+                  <SearchIcon />
+                </IconButton>
+              }
             </InputAdornment>
           )
         }}
